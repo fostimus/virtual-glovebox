9615787b54e3bc39b08ad330d2310574ab03df24
Derek Foster <derekfoster94@gmail.com>
2021-02-10 10:24:46 -0800

    tailwind stuff

--- App.js
+++ App.js
@@ -5,12 +5,12 @@ import { View, Text, TextInput } from "react-native-tailwind";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-300">
+    <View className="flex-1  items-center justify-center bg-gray-300 lg:bg-blue-500">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
       <TextInput
-        className="border border-solid focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+        className="border border-dashed lg:border-solid  w-48 focus:ring-2 focus:ring-purple-600 focus:border-transparent"
         placeholder="Type here"
       />
     </View>
