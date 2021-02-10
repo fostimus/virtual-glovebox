a2025dc03dbba0579453de37e76b0893d94b04d3
Derek Foster <derekfoster94@gmail.com>
2021-02-10 13:41:22 -0800

    using new tailwind react native library, has more supported styles. starting to come along~

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 // import { StyleSheet, Text, View } from "react-native";
-import { View, Text, TextInput } from "react-native-tailwind";
+import { View, Text, TextInput, Image } from "react-native";
+import tailwind from "tailwind-rn";
 import SignInOption from "./components/SignInOption";
 
 export default function App() {
   return (
-    <View className="flex-1  items-center justify-center bg-gray-300 lg:bg-blue-500">
+    <View
+      style={tailwind(
+        "flex-1  items-center justify-center bg-gray-300 lg:bg-blue-500"
+      )}
+    >
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
 
       <TextInput
-        className="border border-dashed lg:border-solid  w-48 focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+        style={tailwind(
+          "border border-dashed lg:border-solid  w-48 focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+        )}
         placeholder="Type here"
       />
       <SignInOption text="Sign In with Email" />
--- components/SignInOption/SignInOption.js
+++ components/SignInOption/SignInOption.js
@@ -1,15 +1,19 @@
 import React from "react";
 // import { StyleSheet, Text, View } from "react-native";
-import { View, Text, TextInput, Image } from "react-native-tailwind";
+import { View, Text, TextInput, Image } from "react-native";
+import tailwind from "tailwind-rn";
 import email from "./email.png";
 
 export default function SignInOption({ text }) {
   return (
-    <>
+    <View
+      style={tailwind(
+        "flex flex-row container justify-evenly items-center w-60 border border-solid border-black bg-blue-300 p-2 rounded"
+      )}
+    >
       <Image source={email} />
-      <Text className="w-60 border border-solid border-black bg-blue-300">
-        {text}
-      </Text>
-    </>
+      {/* <Text>{text}</Text> */}
+      <Text>{text}</Text>
+    </View>
   );
 }
