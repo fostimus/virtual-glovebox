3f206054934e8d57cbecf8b7a3bee0408159943f
Derek Foster <derekfoster94@gmail.com>
2021-02-10 15:37:29 -0800

    set up fonts for easy use throughout app

--- components/base/AppText.js
+++ components/base/AppText.js
@@ -5,32 +5,27 @@ import baseValues from "./values";
 import {
   useFonts,
   Roboto_400Regular,
-  Roboto_700Bold
+  Roboto_400Regular_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic
 } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
 
-export default function AppText({ style, bold, children }) {
+export default function AppText({ style, bold, italic, children }) {
   let [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
+    Roboto_400Regular_Italic,
+    Roboto_700Bold,
+    Roboto_700Bold_Italic
   });
 
   if (!fontsLoaded) {
     return <AppLoading />;
   }
 
-  let combinedStyles = {
-    ...styles.base,
-    ...style
-  };
+  const appStyles = styleHelper(style, bold, italic);
 
-  if (bold) {
-    combinedStyles = {
-      ...combinedStyles,
-      ...styles.bold
-    };
-  }
-  return <Text style={combinedStyles}>{children}</Text>;
+  return <Text style={appStyles}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
@@ -40,5 +35,43 @@ const styles = StyleSheet.create({
   },
   bold: {
     fontFamily: "Roboto_700Bold"
+  },
+  italic: {
+    fontFamily: "Roboto_400Regular_Italic"
+  },
+  boldItalic: {
+    fontFamily: "Roboto_700Bold_Italic"
   }
 });
+
+function styleHelper(parentStyles, bold, italic) {
+  let combinedStyles = {
+    ...styles.base,
+    ...parentStyles
+  };
+
+  if (bold) {
+    combinedStyles = {
+      ...combinedStyles,
+      ...styles.bold
+    };
+  }
+
+  if (italic) {
+    combinedStyles = {
+      ...combinedStyles,
+      ...styles.italic
+    };
+  }
+
+  if (bold && italic) {
+    combinedStyles = {
+      ...combinedStyles,
+      ...styles.boldItalic
+    };
+
+    console.log(combinedStyles);
+  }
+
+  return combinedStyles;
+}
