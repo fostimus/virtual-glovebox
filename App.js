bf39b25bfb666bf81b180d2075d32d26d4627883
Derek Foster <derekfoster94@gmail.com>
2021-02-10 16:17:54 -0800

    started on SignInField

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 // import { StyleSheet, Text, View } from "react-native";
-import { TextInput, Image } from "react-native";
+import { TextInput, Image, Button } from "react-native";
 import { AppText, AppView } from "./components/base";
 import tailwind from "tailwind-rn";
 import SignIn from "./components/SignIn";
 
+const Stack = createStackNavigator();
+//TODO: intro react context to pass down navigation info to buttons on edge
+
 export default function App() {
   return (
     <NavigationContainer>
-      <AppView
-        style={tailwind("flex-1 items-center justify-center bg-gray-300")}
-      >
-        <SignIn options={["Email", "Google", "Apple"]} />
-      </AppView>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={() => <Home />}
+          options={{ title: "Welcome" }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function Home() {
+  return (
+    <AppView style={tailwind("flex-1 items-center justify-center bg-gray-300")}>
+      <SignIn options={["Email", "Google", "Apple"]} />
+    </AppView>
   );
 }
--- components/SignIn/SignInOptionContainer.js
+++ components/SignIn/SignInOptionContainer.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextInput, Image } from "react-native";
 import { AppView } from "../base";
 import tailwind from "tailwind-rn";
 import SignInOption from "./SignInOption";
+import SignInField from "./SignInField";
 
 export default function SingInOptionContainer({ options }) {
+  const [emailSignIn, setEmailSignIn] = useState(false);
+
+  const placeholders = ["Name", "Email", "Password"];
+
+  //TODO: replace contents with SignInField to test/develop the SignInField more
   return (
     <AppView style={tailwind("")}>
       {options.map(option => (
