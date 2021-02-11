f50fbbe6551be87f8a6a577b25603d6155b59c86
Derek Foster <derekfoster94@gmail.com>
2021-02-10 16:01:27 -0800

    added navigation, refactored App.js

--- App.js
+++ App.js
@@ -1,27 +1,23 @@
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 // import { StyleSheet, Text, View } from "react-native";
-import { View, Text, TextInput, Image } from "react-native";
+import { TextInput, Image } from "react-native";
+import { AppText, AppView } from "./components/base";
 import tailwind from "tailwind-rn";
 import SignIn from "./components/SignIn";
 
 export default function App() {
   return (
-    <View
-      style={tailwind(
-        "flex-1  items-center justify-center bg-gray-300 lg:bg-blue-500"
-      )}
-    >
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      <TextInput
-        style={tailwind(
-          "border border-dashed lg:border-solid  w-48 focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-        )}
-        placeholder="Type here"
-      />
-      <SignIn options={["Email", "Google", "Apple"]} />
-    </View>
+    <NavigationContainer>
+      <AppView
+        style={tailwind("flex-1 items-center justify-center bg-gray-300")}
+      >
+        <SignIn options={["Email", "Google", "Apple"]} />
+      </AppView>
+    </NavigationContainer>
   );
 }
