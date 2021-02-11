6bfdda203be24e418ef10efe1c25e2617e4fe1b5
Derek Foster <derekfoster94@gmail.com>
2021-02-11 15:13:34 -0800

    changed app root to be src/App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { registerRootComponent } from "expo";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -5,9 +6,9 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 // import { StyleSheet, Text, View } from "react-native";
 import { TextInput, Image, Button } from "react-native";
-import { AppText, AppView, AppButton } from "./components/base";
+import { AppText, AppView, AppButton } from "../components/base";
 import tailwind from "tailwind-rn";
-import SignIn from "./components/SignIn";
+import SignIn from "../components/SignIn";
 
 const Stack = createStackNavigator();
 //TODO: intro react context to pass down navigation info to buttons on edge
@@ -21,7 +22,7 @@ const Stack = createStackNavigator();
 //   appId: "1:428139105723:web:84d388ad87152503b0889e"
 // }
 // const fuego = new Fuego(firebaseConfig)
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -43,3 +44,5 @@ function Home() {
     </AppView>
   );
 }
+
+export default registerRootComponent(App);
