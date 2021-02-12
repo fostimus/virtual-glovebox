207b4a1c68f94d7565059db0302960b30fa7bfdd
Derek Foster <derekfoster94@gmail.com>
2021-02-12 12:05:48 -0800

    edited babel config to allow screens, refactored

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import React from "react";
+
 import { registerRootComponent } from "expo";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React from "react";
-import { Home } from "./screens";
+import { Home } from "screens";
 
 const Stack = createStackNavigator();
 //TODO: intro react context to pass down navigation info to buttons on edge
