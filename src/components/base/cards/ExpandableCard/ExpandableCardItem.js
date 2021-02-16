23e40436f9ff65915ee3475a480aacd99aeac4d4
Derek Foster <derekfoster94@gmail.com>
2021-02-15 17:13:14 -0800

    touchable card sections

--- src/components/base/cards/ExpandableCard/ExpandableCardItem.js
+++ src/components/base/cards/ExpandableCard/ExpandableCardItem.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppView, AppText } from "base";
-import { TouchableWithoutFeedback, View } from "react-native";
+import {
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  View,
+  Image
+} from "react-native";
+import closeX from "./close.png";
 import tailwind from "tailwind";
 
 export default function ExpandableCardItem({ title, borderBottom, topBorder }) {
+  const [expanded, setExpanded] = useState(false);
+
   const baseBorderStyles = " border-solid border-vgb-card-border";
 
   let borderStyles = baseBorderStyles;
@@ -16,11 +24,27 @@ export default function ExpandableCardItem({ title, borderBottom, topBorder }) {
 
   return (
     <TouchableWithoutFeedback
-      style={tailwind("w-full")}
-      onPress={() => console.log("yeeeeeha")}
+      style={tailwind("w-full relative")}
+      onPress={() => setExpanded(!expanded)}
     >
       <View style={tailwind(borderStyles + " w-full bg-vgb-white p-2")}>
         <AppText style={tailwind("text-xl ml-6")}>{title}</AppText>
+        {expanded && (
+          <>
+            <TouchableOpacity
+              style={tailwind("absolute right-4 top-4")}
+              onPress={() => setExpanded(!expanded)}
+            >
+              <Image source={closeX} />
+            </TouchableOpacity>
+            <View>
+              <AppText>Lorem Ipsum</AppText>
+              <AppText>Lorem Flipsum</AppText>
+              <AppText>Lorem Yipsum</AppText>
+              <AppText>Full {title}</AppText>
+            </View>
+          </>
+        )}
       </View>
     </TouchableWithoutFeedback>
   );
