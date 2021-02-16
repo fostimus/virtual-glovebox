86d8b50962592287a118c6e09671d264afb09de7
Derek Foster <derekfoster94@gmail.com>
2021-02-15 16:25:02 -0800

    v1 of card for quick Id

--- src/components/base/cards/ExpandableCard/ExpandableCard.js
+++ src/components/base/cards/ExpandableCard/ExpandableCard.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { AppView } from "base";
+import { AppView, AppText } from "base";
 import AppCard from "../AppCard";
 import ExpandableCardItem from "./ExpandableCardItem";
 import tailwind from "tailwind";
 
 export default function ExpandableCard({ title, items }) {
   return (
-    <AppCard title={title} titleStyles={tailwind("w-full")}>
-      {items.map(item => (
-        <ExpandableCardItem title={item} />
+    <AppView
+      style={tailwind(
+        "flex items-center justify-center w-11/12 rounded-md my-2"
+      )}
+    >
+      <AppView style={tailwind("w-full bg-vgb-white rounded-md")}>
+        <AppText bold style={tailwind("w-full text-xl ml-4 underline")}>
+          {title}
+        </AppText>
+      </AppView>
+      {items.map((item, index) => (
+        <ExpandableCardItem
+          title={item}
+          borderBottom={index === items.length - 1}
+        />
       ))}
-    </AppCard>
+    </AppView>
   );
 }
--- src/components/base/cards/ExpandableCard/ExpandableCardItem.js
+++ src/components/base/cards/ExpandableCard/ExpandableCardItem.js
@@ -2,10 +2,17 @@ import React from "react";
 import { AppView, AppText } from "base";
 import tailwind from "tailwind";
 
-export default function ExpandableCardItem({ title }) {
+export default function ExpandableCardItem({ title, borderBottom }) {
+  let borderStyles = "border-t-2 border-r-2 border-l-2";
+  if (borderBottom) borderStyles = borderStyles + " border-b-2";
   return (
-    <AppView style={tailwind("border border-solid w-full")}>
-      <AppText>{title}</AppText>
+    <AppView
+      style={tailwind(
+        borderStyles +
+          " rounded-md border-solid w-full border-vgb-card-border bg-vgb-white"
+      )}
+    >
+      <AppText style={tailwind("text-xl ml-6")}>{title}</AppText>
     </AppView>
   );
 }
