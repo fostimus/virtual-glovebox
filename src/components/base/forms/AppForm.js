a7e1e15714b9ba85b367945331f3f7899ce54719
Derek Foster <derekfoster94@gmail.com>
2021-02-17 12:22:54 -0800

    refactored form to be it's own reuable component

--- src/components/base/forms/AppForm.js
+++ src/components/base/forms/AppForm.js
@@ -1,72 +1,33 @@
 import React, { useState } from "react";
-import Screen from "../Screen";
-import { AppTitle, AppView, AppText } from "base";
+import { AppView, AppText } from "base";
 import { AppButton, CancelButton } from "base/buttons";
 import { AppCard } from "base/cards";
 import DropDownPicker from "react-native-dropdown-picker";
 import { TouchableOpacity, Image, TextInput } from "react-native";
 import tailwind from "tailwind";
 
-export default function AppForm({ title }) {
+export default function AppForm({ title, rows }) {
   /*
-   * rows: [{}]
+   * rows: [[{name: "Make" type: "dropdown", options: []},{}],[],[{name: "Vehicle Identification Number (VIN)"}]]
    */
 
   return (
     <AppCard title={title}>
-      <FormRow>
-        <FormItem name="Make">
-          <Dropdown
-            options={[
-              { label: "Honda", value: "Honda" },
-              { label: "Toyota", value: "Toyota" },
-              { label: "Tesla", value: "Tesla" }
-            ]}
-          />
-        </FormItem>
-        <FormItem name="Model">
-          <Dropdown
-            options={[
-              { label: "Honda", value: "Honda" },
-              { label: "Toyota", value: "Toyota" },
-              { label: "Tesla", value: "Tesla" }
-            ]}
-          />
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem name="Year">
-          <Dropdown
-            small
-            options={[
-              { label: "2010", value: "2010" },
-              { label: "2011", value: "2011" },
-              { label: "2012", value: "2012" }
-            ]}
-          />
-        </FormItem>
-        <FormItem name="License Plate">
-          <FormInput large />
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem single name="Vehicle Identification Number (VIN)">
-          <FormInput full />
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem single name="Registered Owner">
-          <FormInput full />
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem name="Reg. Valid From">
-          <FormInput />
-        </FormItem>
-        <FormItem name="Reg. Valid To">
-          <FormInput />
-        </FormItem>
-      </FormRow>
+      {rows.map(row => (
+        <FormRow>
+          {row.map(item => (
+            <FormItem
+              name={item.name}
+              small={item.small}
+              large={item.large}
+              full={row.length === 1}
+              type={item.type}
+              options={item.options}
+            />
+          ))}
+        </FormRow>
+      ))}
+
       <AppView
         style={tailwind(
           "flex flex-row w-full justify-evenly border-t border-solid border-gray-400"
@@ -79,56 +40,55 @@ export default function AppForm({ title }) {
   );
 }
 
-function FormItem({ name, children, single }) {
-  let style = "";
-  if (single) style = "w-full";
+function FormRow({ children }) {
   return (
-    <AppView style={tailwind(style)}>
-      <AppText>
-        {name}: <AppText style={tailwind("text-red-500")}>*</AppText>
-      </AppText>
+    <AppView
+      style={tailwind("w-full flex flex-row justify-between pb-2 pl-4 pr-4")}
+    >
       {children}
     </AppView>
   );
 }
 
-function Dropdown({ small, large, options }) {
+function FormItem({ small, large, full, name, type, options, children }) {
   const [value, setValue] = useState("");
 
   let width = "w-36";
   if (small) width = "w-20";
   if (large) width = "w-52";
-
-  return (
-    <DropDownPicker
-      defaultValue={null}
-      placeholder=""
-      containerStyle={tailwind(width + " h-10 my-1")}
-      style={tailwind("bg-white border-gray-400 rounded-lg")}
-      itemStyle={{
-        justifyContent: "flex-start"
-      }}
-      dropDownStyle={tailwind("absolute z-50 bg-blue-500")}
-      items={options}
-      onChangeItem={item => setValue(item.value)}
-    />
-  );
-}
-
-function FormInput({ small, large, full }) {
-  let width = "w-36";
-  if (small) width = "w-20";
-  if (large) width = "w-52";
   if (full) width = "w-full";
 
-  return (
-    <AppView style={tailwind("w-full")}>
-      <TextInput
-        style={tailwind(
-          width +
-            " my-1 bg-white h-10 border border-solid border-gray-400 rounded-lg"
-        )}
+  const child =
+    type === "dropdown" ? (
+      <DropDownPicker
+        defaultValue={null}
+        placeholder=""
+        containerStyle={tailwind(width + " h-10 my-1")}
+        style={tailwind("bg-white border-gray-400 rounded-lg")}
+        itemStyle={{
+          justifyContent: "flex-start"
+        }}
+        dropDownStyle={tailwind("absolute z-50 bg-blue-500")}
+        items={options}
+        onChangeItem={item => setValue(item.value)}
       />
+    ) : (
+      <AppView style={tailwind("w-full")}>
+        <TextInput
+          style={tailwind(
+            width +
+              " my-1 bg-white h-10 border border-solid border-gray-400 rounded-lg"
+          )}
+        />
+      </AppView>
+    );
+
+  return (
+    <AppView style={tailwind(width)}>
+      <AppText>
+        {name}: <AppText style={tailwind("text-red-500")}>*</AppText>
+      </AppText>
+      {child}
     </AppView>
   );
 }
