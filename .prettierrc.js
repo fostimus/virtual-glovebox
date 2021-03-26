c39835853c175c9a4a0176da67361ae6207d9904
Derek Foster <derekfoster94@gmail.com>
2021-03-25 21:54:19 -0700

    added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "never",
+  //NOTE: higher print width will allow custom atom-tailwind syntax suggestions. Is the change in formatting worth that benefit?
+  printWidth: 120,
+};
